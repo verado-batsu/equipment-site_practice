c160daa9903208a2ff5c093993f566bf8e01ff2d
Vladyslav <kupchuk.vlad@gmail.com>
2024-05-26 02:48:49 +0300

    ADDED: equipment details page and mui card styles

--- src/components/EquipmentList/EquipmentList.jsx
+++ src/components/EquipmentList/EquipmentList.jsx
@@ -1,5 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 import styles from './EquipmentList.module.scss';
 const {
     listWrapper,
@@ -16,19 +22,39 @@ export function EquipmentList({ equipments }) {
             <ul className={equipmentList}>
                 {equipments ? (
                     equipments.map(equipment => (
-                        <li key={equipment.id} className={equipmentItem}>
+                        <li
+                            key={equipment.id}
+                            // className={equipmentItem}
+                        >
                             <Link
                                 to={`${equipment.id}`}
                                 className={equipmentLink}
                             >
-                                <img
-                                    className={equipmentImg}
-                                    src={equipment.photos[0]}
-                                    alt="equipment_photo"
-                                />
-                                <h3 className={equipmentTitle}>
-                                    {equipment.model}
-                                </h3>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardMedia
+                                        component="img"
+                                        alt={equipment.model}
+                                        height="140"
+                                        image={equipment.photos[0]}
+                                    />
+                                    <CardContent>
+                                        {/* <img
+                                        className={equipmentImg}
+                                        src={equipment.photos[0]}
+                                        alt={equipment.model}
+                                    /> */}
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="div"
+                                        >
+                                            {equipment.model}
+                                        </Typography>
+                                        {/* <h3 className={equipmentTitle}>
+                                            {equipment.model}
+                                        </h3> */}
+                                    </CardContent>
+                                </Card>
                             </Link>
                         </li>
                     ))
--- src/pages/EquipmentDetails/EquipmentDetails.jsx
+++ src/pages/EquipmentDetails/EquipmentDetails.jsx
@@ -1,10 +1,45 @@
+import { useParams } from 'react-router-dom';
+
+import { getEquipmentById } from 'api/fakeAPI';
+
 import styles from './EquipmentDetails.module.scss';
-const { equipmentDetails } = styles;
+const {
+    equipmentDetails,
+    equipmentImg,
+    equipmentInfo,
+    equipmentTitle,
+    equipmentFeatures,
+    equipmentDescr,
+} = styles;
 
 export function EquipmentDetails() {
+    const { equipmentId } = useParams();
+    const equipment = getEquipmentById(equipmentId);
+
     return (
         <section className={equipmentDetails}>
-            <div className="container">Equipment Details</div>
+            <div className="container">
+                <img
+                    className={equipmentImg}
+                    src={equipment?.photos[0] || ''}
+                    alt={equipment?.model || ''}
+                />
+                <div className={equipmentInfo}>
+                    <h2 className={equipmentTitle}>{equipment?.model || ''}</h2>
+                    <p
+                        className={equipmentFeatures}
+                        style={{ whiteSpace: 'pre-wrap' }}
+                    >
+                        {equipment?.features || ''}
+                    </p>
+                    <p
+                        className={equipmentDescr}
+                        style={{ whiteSpace: 'pre-wrap' }}
+                    >
+                        {equipment?.describe || ''}
+                    </p>
+                </div>
+            </div>
         </section>
     );
 }
