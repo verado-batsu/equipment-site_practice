74ac62866bc28091678cd0a2f00519a401500744
Vladyslav <kupchuk.vlad@gmail.com>
2024-12-02 18:46:55 +0200

    ADDED: content to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,27 @@
+import { NavLink } from 'react-router-dom';
+
 import styles from './Home.module.scss';
-const { home } = styles;
+const { home, homeTitle, homeDescription, homeLinkWrapper, homeLink } = styles;
 
 export function HomePage() {
     return (
         <section className={home}>
-            <div className="container">Home page</div>
+            <div className="container">
+                <h1 className={homeTitle}>
+                    Ласкаво просимо у каталог обладнань для обробки тиском
+                </h1>
+                <p className={homeDescription}>
+                    На сайті ви можете подивитись каталог всіх доданих
+                    обладнань, а також знайти одне конкретне за допомогою
+                    фільтру по категорії обладнання або за допомогою пошуку по
+                    назві.
+                </p>
+                <div className={homeLinkWrapper}>
+                    <NavLink className={homeLink} to="equipments">
+                        До обладання
+                    </NavLink>
+                </div>
+            </div>
         </section>
     );
 }
